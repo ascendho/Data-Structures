717f2ad6d0ed78f7d49d11c8290def2855903fb4
Ascend Ho <coderhs@foxmail.com>
2024-03-11 20:25:19 +0800

    keep clean

--- 02-Linear List/TextBook/Difference/Difference.h
+++
@@ -1,8 +0,0 @@
-#ifndef DIFFERENCE_DIFFERENCE_H
-#define DIFFERENCE_DIFFERENCE_H
-
-#include "../SLinkList/SLinkList.h"
-
-void difference(SLinkList &space, int &S);
-
-#endif //DIFFERENCE_DIFFERENCE_H
